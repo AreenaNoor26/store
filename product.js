32c2f22f42165526d995931a1227cc57c868e401
AreenaNoor26 <157211943+AreenaNoor26@users.noreply.github.com>
2026-05-27 00:45:11 +0500

    Add files via upload

---
+++ product.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: 1,
+    name: "Velvet Lipstick",
+    price: 22,
+    category: "Lips",
+    image: "/redlipstick.jpg"
+  },
+  {
+    id: 2,
+    name: "Glow Foundation",
+    price: 30,
+    category: "Face",
+    image: "/foundation.jpg"
+  },
+  {
+    id: 3,
+    name: "Rose Blush",
+    price: 18,
+    category: "Face",
+    image: "/blush.jpg"
+  },
+  {
+    id: 4,
+    name: "Nude Palette",
+    price: 40,
+    category: "Eyes",
+    image: "/Nude.jpg"
+  },
+  {
+    id: 5,
+    name: "Mascara",
+    price: 16,
+    category: "Eyes",
+    image: "/Mascara.jpg"
+  },
+  {
+    id: 6,
+    name: "Lip Gloss",
+    price: 14,
+    category: "Lips",
+    image: "/Lipgloss.jpg"
+  },
+  {
+    id: 7,
+    name: "Concealer",
+    price: 19,
+    category: "Face",
+    image: "/Concealer.jpg"
+  },
+  {
+    id: 8,
+    name: "Beauty Sponge",
+    price: 10,
+    category: "Accessories",
+    image: "/sponage.jpg"
+  }
+];
+
+export default products;
