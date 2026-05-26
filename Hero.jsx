054d0ef8dcea76329604316e1f8a9be9f67e1aa6
AreenaNoor26 <157211943+AreenaNoor26@users.noreply.github.com>
2026-05-27 00:43:57 +0500

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Glow Like Never Before</h1>
+      <p>Luxury beauty essentials for every style</p>
+      <button>Shop Now</button>
+    </section>
+  );
+}
+
+export default Hero;
